09f3acdb8a3d66c8aba88c2bae0439ec5beb1e92
Alejandro Miranda <amiranda110500@gmail.com>
2022-10-20 14:41:54 +0100

    Initial Commit

---
+++ Punk App/Punk_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Punk_AppApp.swift
+//  Punk App
+//
+//  Created by Alejandro Miranda on 20/10/22.
+//
+
+import SwiftUI
+
+@main
+struct Punk_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
